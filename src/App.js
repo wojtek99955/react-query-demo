96ef778b46757307cf80173f00d1c37303f81dd2
Wojtek Książek <wojtek99955@gmail.com>
2022-04-25 20:04:06 +0200

    react-query setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { useQuery } from "react-query";
+
 function App() {
-  return <></>;
+  const { isLoading, error, data } = useQuery("people", () =>
+    fetch("https://swapi.dev/api/people/2/").then((res) => res.json())
+  );
+  console.log(data);
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  return <>{data.name}</>;
 }
 
 export default App;
